bc64e4770bea35144b1ee82516649b6e7ac36926
ittps-pro <147715086+ittps-pro@users.noreply.github.com>
2024-02-02 23:48:14 +0300

    Create index.ts

---
+++ server/index.ts
@@ -0,0 +1,15 @@
+const Server = (config) => {
+
+  let instance = {
+    // auth: 
+    ...config.sso,
+  }
+  // /realms/prod/protocol/saml?SAMLRequest=
+  
+  // lZJda4MwGIX/ine50mhMWw0qSMug0I3RfVzsZmQa10A%2BXN64bv9%2BainrLjYYhFyE8z7n5CQFcK16Vg/%2BYPbibRDggxpAOC%2BtWVsDgxbuTrh32YiH/a5EB%2B97YBgfj8eok6%2BaR43VeKLgPFvRPElonGX5YrnMCcXNiYCCzQiWhk/UbwaAjWwauQHz0R87wZUG3DvbTpu3jVUzGQXbTYme6UuXtqsuC3PB05CmXRrybLEIKeVxt4oJIV06SgEGsTXgufElIjGhYUzGdU9ilmYsyZ5Q8CgczElIFKPgQysDbDIq0eAMsxwkMMO1AOYbdldf79goZPzcy%2BVI//fM%2BR6oKiY1m9O56l8tFvhytDi92M1otd3cWiWbz6BWyh7XY39elMi7YSz8yjrN/e/hkiiZT2QbdrOUCc2lqtvWCQCEq5Prz69RfQE=
+  console.log("sso")
+  return { server: ${hostname}, username: "", token: "" }
+          
+}
+
+// #005BFF
